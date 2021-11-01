65da7b4b846d1a1fc0d663781a5911d5bfa58cb7
Anshul1507 <ag1507anshul@gmail.com>
2021-11-01 17:40:31 +0530

    init stt engine

--- app/src/main/java/com/example/foregroundservice/MainActivity.kt
+++ app/src/main/java/com/example/foregroundservice/MainActivity.kt
@@ -4,19 +4,30 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.*
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.foregroundservice.STT.Stt
+import com.example.foregroundservice.STT.SttListener
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var stt: Stt
+    }
 
     private val PERMISSION_REQUEST_RECORD_AUDIO = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setup stt engine
+        initSttEngine()
+
         // check for permission
         val permissionCheck =
             ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.RECORD_AUDIO)
@@ -29,7 +40,25 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
+
     }
+
+    private fun initSttEngine() {
+        stt = Stt(application, supportFragmentManager, object : SttListener {
+            override fun onSttLiveSpeechResult(liveSpeechResult: String) {
+                Log.d(application.packageName, "Speech result - $liveSpeechResult")
+            }
+
+            override fun onSttFinalSpeechResult(speechResult: String) {
+                Log.d(application.packageName, "Final speech result - $speechResult")
+            }
+
+            override fun onSttSpeechError(errMsg: String) {
+                Log.d(application.packageName, "Speech error - $errMsg")
+            }
+        })
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
